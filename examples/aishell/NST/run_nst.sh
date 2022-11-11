77c8f1adbefb0e1345b1ad3624933c4ba37de87f
yuch <cy823236858@gmail.com>
2022-11-11 19:32:47 +0800

    add run_nst.sh

---
+++ examples/aishell/NST/run_nst.sh
@@ -0,0 +1,504 @@
+#!/bin/bash
+
+# Copyright 2019 Mobvoi Inc. All Rights Reserved.
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Augmented version of original run.sh to make the code compatible with noisy student training
+
+. ./path.sh || exit 1;
+
+# Use this to control how many gpu you use, It's 1-gpu training if you specify
+# just 1gpu, otherwise it's is multiple gpu training based on DDP in pytorch
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+#export CUDA_VISIBLE_DEVICES="0"
+# The NCCL_SOCKET_IFNAME variable specifies which IP interface to use for nccl
+# communication. More details can be found in
+# https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/env.html
+# export NCCL_SOCKET_IFNAME=ens4f1
+export NCCL_DEBUG=INFO
+stage=0 # start from 0 if you need to start from data preparation
+stop_stage=5
+
+# here are extra parameters for NST
+data_list_dir=""
+job_num=-1
+wav_label_dir=""
+cer_out_dir=""
+text_file=""
+hypo_name=""
+dir=""
+pseudo_data_list=""
+gcmvn=""
+checkpoint=
+average_num=30
+nj=16
+#exp/conformer_wenet1k_nst0_sr3_v2/avg_30.pt
+# The num of machines(nodes) for multi-machine training, 1 is for one machine.
+# NFS is required if num_nodes > 1.
+#global_cmvn_ai12
+
+num_nodes=1
+
+# The rank of each node or machine, which ranges from 0 to `num_nodes - 1`.
+# You should set the node_rank=0 on the first machine, set the node_rank=1
+# on the second machine, and so on.
+node_rank=0
+# data
+data=/aishell1
+data_url=www.openslr.org/resources/33
+
+dict=data/dict/lang_char.txt
+
+# data_type can be `raw` or `shard`. Typically, raw is used for small dataset,
+# `shard` is used for large dataset which is over 1k hours, and `shard` is
+# faster on reading data and training.
+data_type=shard
+num_utts_per_shard=1000
+
+train_set=train
+# Optional train_config
+# 1. conf/train_transformer.yaml: Standard transformer
+# 2. conf/train_conformer.yaml: Standard conformer
+# 3. conf/train_unified_conformer.yaml: Unified dynamic chunk causal conformer
+# 4. conf/train_unified_transformer.yaml: Unified dynamic chunk transformer
+# 5. conf/train_u2++_conformer.yaml: U2++ conformer
+# 6. conf/train_u2++_transformer.yaml: U2++ transformer
+
+
+train_config=conf/train_conformer_yuch_ratio2.yaml
+
+cmvn=true
+#dir=exp/conformer_wenet1k_nst5_LM_diff_leq_10
+#checkpoint=exp/conformer_wenet1k_nst0_sr3_v2/avg_30.pt
+
+#avg_30.pt
+
+# use average_checkpoint will get better result
+# ??
+average_checkpoint=true
+target_pt=80
+decode_checkpoint=$dir/$target_pt.pt
+
+# here we only use attention_rescoring for NST
+decode_modes="attention_rescoring"
+#decode_modes="ctc_greedy_search ctc_prefix_beam_search attention attention_rescoring"
+
+
+. tools/parse_options.sh || exit 1;
+
+# print the settings
+echo "batch = 32"
+echo "setting for this run"
+echo "dir is ${dir}"
+echo "pseudo data list is ${pseudo_data_list}"
+echo "data_list_dir is ${data_list_dir}"
+echo "job_num is ${job_num}"
+echo "wav_label_dir is ${wav_label_dir}"
+echo "cer_out_dir is  ${cer_out_dir}"
+echo "text_file is ${text_file}"
+echo "average_num is ${average_num}"
+echo "checkpoint is ${checkpoint} "
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+  echo "stage -1: Data Download"
+  local/download_and_untar.sh ${data} ${data_url} data_aishell
+  local/download_and_untar.sh ${data} ${data_url} resource_aishell
+fi
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+  # Data preparation
+  local/aishell_data_prep.sh ${data}/data_aishell/wav \
+    ${data}/data_aishell/transcript
+fi
+
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+  # remove the space between the text labels for Mandarin dataset
+  for x in train dev test; do
+    cp data/${x}/text data/${x}/text.org
+    paste -d " " <(cut -f 1 -d" " data/${x}/text.org) \
+      <(cut -f 2- -d" " data/${x}/text.org | tr -d " ") \
+      > data/${x}/text
+    rm data/${x}/text.org
+  done
+
+  # one should calculate global_cmvn for whatever dataset they use (combining supervised and unsupervisde).
+  # replace the name for --in_scp and --out_cmvb recording to your dataset.
+  tools/compute_cmvn_stats.py --num_workers 16 --train_config $train_config \
+    --in_scp data/${train_set}/ai12_wav.scp \
+    --out_cmvn data/$train_set/global_cmvn_ai12
+fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+  echo "Make a dictionary"
+  mkdir -p $(dirname $dict)
+  echo "<blank> 0" > ${dict}  # 0 is for "blank" in CTC
+  echo "<unk> 1"  >> ${dict}  # <unk> must be 1
+  tools/text2token.py -s 1 -n 1 data/train/text | cut -f 2- -d" " \
+    | tr " " "\n" | sort | uniq | grep -a -v -e '^\s*$' | \
+    awk '{print $0 " " NR+1}' >> ${dict}
+  num_token=$(cat $dict | wc -l)
+  echo "<sos/eos> $num_token" >> $dict
+fi
+
+
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+  echo "Prepare data, prepare requried format"
+  for x in dev test ${train_set}; do
+    if [ $data_type == "shard" ]; then
+      tools/make_shard_list.py --num_utts_per_shard $num_utts_per_shard \F
+        --num_threads 16 data/$x/wav.scp data/$x/text \
+        $(realpath data/$x/shards) data/$x/data.list
+    else
+      tools/make_raw_list.py data/$x/wav.scp data/$x/text \
+        data/$x/data.list
+    fi
+  done
+fi
+
+# This stage is modified for NST
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
+  mkdir -p $dir
+  # You have to rm `INIT_FILE` manually when you resume or restart a
+  # multi-machine training.
+  rm $dir/ddp_init
+  INIT_FILE=$dir/ddp_init
+  init_method=file://$(readlink -f $INIT_FILE)
+  echo "$0: init method is $init_method"
+  num_gpus=$(echo $CUDA_VISIBLE_DEVICES | awk -F "," '{print NF}')
+  # Use "nccl" if it works, otherwise use "gloo"
+  dist_backend="gloo"
+  world_size=`expr $num_gpus \* $num_nodes`
+  echo "total gpus is: $world_size"
+  cmvn_opts=
+  $cmvn && cp data/${train_set}/global_cmvn $dir/global_cmvn
+  $cmvn && cmvn_opts="--cmvn ${dir}/global_cmvn"
+
+  # train.py rewrite $train_config to $dir/train.yaml with model input
+  # and output dimension, and $dir/train.yaml will be used for inference
+  # and export.
+  echo "checkpoint is "  ${checkpoint}
+  for ((i = 0; i < $num_gpus; ++i)); do
+  {
+    gpu_id=$(echo $CUDA_VISIBLE_DEVICES | cut -d',' -f$[$i+1])
+    echo "gpu number  $i "
+    # Rank of each gpu/process used for knowing whether it is
+    # the master of a worker.
+    #pseudo_data_6_5_nst_2_hypo_leq10.list
+    rank=`expr $node_rank \* $num_gpus + $i`
+
+    # we added several new arugment for train.py
+    # "--train_data_supervised" is the path of datalist for supervised data, which refers to
+    # aishell-1 in the paper. One can modify it manually or feed
+    #
+    python wenet/bin/train.py --gpu $gpu_id \
+      --config $train_config \
+      --data_type $data_type \
+      --symbol_table $dict \
+      --train_data_supervised data/$train_set/data_aishell.list \
+      --train_data_wenetspeech data/$train_set/$pseudo_data_list \
+      --cv_data data/dev/data.list \
+      ${checkpoint:+--checkpoint $checkpoint} \
+      --model_dir $dir \
+      --ddp.init_method $init_method \
+      --ddp.world_size $world_size \
+      --ddp.rank $rank \
+      --ddp.dist_backend $dist_backend \
+      --num_workers 1 \
+      --dataset_num 2 \
+      --fuse_batch 0 \
+      $cmvn_opts \
+      --pin_memory
+  } &
+  done
+  wait
+fi
+
+
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+  # Test model, please specify the model you want to test by --checkpoint
+  # stage 5 we test with aishell dataset,
+  if [ ${average_checkpoint} == true ]; then
+    decode_checkpoint=$dir/avg_${average_num}.pt
+    echo "do model average and final checkpoint is $decode_checkpoint"
+    python wenet/bin/average_model.py \
+      --dst_model $decode_checkpoint \
+      --src_path $dir  \
+      --num ${average_num} \
+      --val_best
+  fi
+  # Please specify decoding_chunk_size for unified streaming and
+  # non-streaming model. The default value is -1, which is full chunk
+  # for non-streaming inference.
+  decoding_chunk_size=
+  ctc_weight=0.5
+  reverse_weight=0.0
+
+  # test_wer
+  for mode in ${decode_modes}; do
+  {
+    #test_dir=$dir/test_${mode}_${target_pt}pt  # for target pt
+    test_dir=$dir/test_${mode}_ep240_${average_num}pt   # for average pt
+    mkdir -p $test_dir
+    python wenet/bin/recognize.py --gpu 0 \
+      --mode $mode \
+      --config $dir/train.yaml \
+      --data_type $data_type \
+      --test_data data/test/data.list \
+      --checkpoint $decode_checkpoint \
+      --beam_size 10 \
+      --batch_size 1 \
+      --penalty 0.0 \
+      --dict $dict \
+      --ctc_weight $ctc_weight \
+      --reverse_weight $reverse_weight \
+      --result_file $test_dir/text \
+      ${decoding_chunk_size:+--decoding_chunk_size $decoding_chunk_size}
+    echo "before compute-wer"
+    python tools/compute-wer.py --char=1 --v=1 \
+      data/test/text $test_dir/text > $test_dir/wer
+  } &
+  done
+
+  # dev_wer
+#  for mode in ${decode_modes}; do
+#  {
+#    #test_dir=$dir/test_${mode}_${target_pt}pt  # for target pt
+#    dev_dir=$dir/dev_${mode}_ep240_${average_num}pt   # for average pt
+#    mkdir -p $dev_dir
+#    python wenet/bin/recognize.py --gpu 0 \
+#      --mode $mode \
+#      --config $dir/train.yaml \
+#      --data_type $data_type \
+#      --test_data data/dev/data.list \
+#      --checkpoint $decode_checkpoint \
+#      --beam_size 10 \
+#      --batch_size 1 \
+#      --penalty 0.0 \
+#      --dict $dict \
+#      --ctc_weight $ctc_weight \
+#      --reverse_weight $reverse_weight \
+#      --result_file $dev_dir/text \
+#      ${decoding_chunk_size:+--decoding_chunk_size $decoding_chunk_size}
+#    echo "before compute-wer"
+#    python tools/compute-wer.py --char=1 --v=1 \
+#      data/dev/text $dev_dir/text > $dev_dir/wer
+#  } &
+#  done
+  wait
+fi
+
+
+if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
+  # Test model, please specify the model you want to test by --checkpoint
+  # stage 5 we test with aishell dataset,
+
+  decode_checkpoint=$dir/avg_${average_num}.pt
+  echo "do model average and final checkpoint is $decode_checkpoint"
+  decoding_chunk_size=
+  ctc_weight=0.5
+  reverse_weight=0.0
+
+  # dev_wer
+  for mode in ${decode_modes}; do
+  {
+    #test_dir=$dir/test_${mode}_${target_pt}pt  # for target pt
+    dev_dir=$dir/dev_${mode}_ep240_${average_num}pt   # for average pt
+    mkdir -p $dev_dir
+    python wenet/bin/recognize.py --gpu 0 \
+      --mode $mode \
+      --config $dir/train.yaml \
+      --data_type $data_type \
+      --test_data data/dev/data.list \
+      --checkpoint $decode_checkpoint \
+      --beam_size 10 \
+      --batch_size 1 \
+      --penalty 0.0 \
+      --dict $dict \
+      --ctc_weight $ctc_weight \
+      --reverse_weight $reverse_weight \
+      --result_file $dev_dir/text \
+      ${decoding_chunk_size:+--decoding_chunk_size $decoding_chunk_size}
+    echo "before compute-wer"
+    python tools/compute-wer.py --char=1 --v=1 \
+      data/dev/text $dev_dir/text > $dev_dir/wer
+  } &
+  done
+  wait
+fi
+
+
+
+if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
+  # Export the best model you want
+  python wenet/bin/export_jit.py \
+    --config $dir/train.yaml \
+    --checkpoint $dir/avg_${average_num}.pt \
+    --output_file $dir/final.zip \
+    --output_quant_file $dir/final_quant.zip
+fi
+
+# Optionally, you can add LM and test it with runtime.
+if [ ${stage} -le 22 ] && [ ${stop_stage} -ge 22 ]; then
+#   #7.1 Prepare dict
+#  unit_file=$dict
+#  mkdir -p data/local/dict
+#  cp $unit_file data/local/dict/units.txt
+#  tools/fst/prepare_dict.py $unit_file data/resource_aishell/lexicon.txt \
+#    data/local/dict/lexicon.txt
+#   #7.2 Train lm
+#  lm=data/local/lm
+#  mkdir -p $lm
+#  tools/filter_scp.pl data/train/text \
+#    $data/transcript/aishell_transcript_v0.8.txt > $lm/text
+  echo "step0"
+  local/aishell_train_lms.sh
+#  # 7.3 Build decoding TLG
+  echo "step1"
+  tools/fst/compile_lexicon_token_fst.sh \
+    data/local/dict data/local/tmp data/local/lang
+  echo "step2"
+  tools/fst/make_tlg.sh data/local/lm data/local/lang data/lang_test_aishell2 || exit 1;
+  echo "step3"
+  # 7.4 Decoding with runtime
+#  echo "start "
+#  now=$(date +"%T")
+#  echo "Current time : $now"
+#  chunk_size=-1
+#  ./tools/decode.sh --nj 6 \
+#    --beam 15.0 --lattice_beam 7.5 --max_active 7000 \
+#    --blank_skip_thresh 0.98 --ctc_weight 0.5 --rescoring_weight 1.0 \
+#    --chunk_size $chunk_size \
+#    --fst_path data/lang_test/TLG.fst \
+#    data/test/wav.scp data/test/text $dir/final.zip \
+#    data/lang_test/words.txt $dir/lm_with_runtime_split_6
+#  echo "end"
+#  now=$(date +"%T")
+#  echo "Current time : $now"
+#   Please see $dir/lm_with_runtime for wer
+fi
+
+
+# here i add extra stages for Noisy Student Training
+
+#first we split the wenetspeech datalist into N sublists
+if [ ${stage} -le 8 ] && [ ${stop_stage} -ge 8 ]; then
+  # Export the best model you want
+  python split_data_list.py \
+    --job_nums 100 \
+    --data_list_path data/train/data_wenet.list
+
+fi
+
+# stage 9 will perform inference on the given sublist(job num)
+# one need to specify the job_num and the test_dir
+# here is example usages:
+# bash run_nst_1.sh --stage 9 --stop-stage 9 --job_num 0 --data_list_dir data/train/data_LM_diff_10/
+# bash run_nst_1.sh --stage 9 --stop-stage 9 --job_num 0 --data_list_dir data/train/data_wenet_1k_split_100/
+# --data_list_dir data/train/data_LM_diff_10/
+if [ ${stage} -le 9 ] && [ ${stop_stage} -ge 9 ]; then
+  echo "start time : $now"
+  # Test model, please specify the model you want to test by --checkpoint
+#  if [ ${average_checkpoint} == true ]; then
+#    decode_checkpoint=$dir/avg_${average_num}.pt
+#    echo "do model average and final checkpoint is $decode_checkpoint"
+#    python wenet/bin/average_model.py \
+#      --dst_model $decode_checkpoint \
+#      --src_path $dir  \
+#      --num ${average_num} \
+#      --val_best
+#  fi
+  # we assume you have run stage 5 so that avg_${average_num}.pt exists
+  decode_checkpoint=$dir/avg_${average_num}.pt
+  # Please specify decoding_chunk_size for unified streaming and
+  # non-streaming model. The default value is -1, which is full chunk
+  # for non-streaming inference.
+  decoding_chunk_size=
+  ctc_weight=0.5
+  reverse_weight=0.0
+  mode="attention_rescoring"
+  gpu_id=0
+  echo "job number  ${job_num} "
+  echo "data_list dir is  ${data_list_dir}"
+  echo "hypo name is " $hypo_name
+  echo "dir is ${dir}"
+  #test_dir=$dir/hypothesis_without_lm_cer_hypo_filter/test_${mode}_${job_num}
+  #mkdir -p $test_dir
+  python wenet/bin/recognize.py --gpu $gpu_id \
+    --mode $mode \
+    --config $dir/train.yaml \
+    --data_type $data_type \
+    --test_data ${data_list_dir}data_wenet${job_num}/data_list \
+    --checkpoint $decode_checkpoint \
+    --beam_size 10 \
+    --batch_size 1 \
+    --penalty 0.0 \
+    --dict $dict \
+    --ctc_weight $ctc_weight \
+    --reverse_weight $reverse_weight \
+    --result_file ${data_list_dir}data_wenet${job_num}/${hypo_name} \
+    ${decoding_chunk_size:+--decoding_chunk_size $decoding_chunk_size}
+    echo "end time : $now"
+#  python tools/compute-wer.py --char=1 --v=1 \
+#    data/test/text $test_dir/text > $test_dir/wer
+fi
+
+# language model
+#  bash run_nst_1.sh --stage 10 --stop-stage 10 --job_num 0 --wav_label_dir
+if [ ${stage} -le 10 ] && [ ${stop_stage} -ge 10 ]; then
+  chunk_size=-1
+  mode="attention_rescoring"
+  test_dir=$dir/test_${mode}_${job_num}
+  now=$(date +"%T")
+  echo "start time : $now"
+  echo "GPU dir is " $job_num "wav_label_dir is " $wav_label_dir "nj is" $nj "text_file is" $text_file "cer out is" $cer_out_dir "lm is 4gram"
+  echo "dir is " $dir
+  if [ ! -f ${wav_label_dir}data_wenet${job_num}/${text_file}  ]; then
+  echo "text file does not exists"
+  exit 1;
+  fi
+
+  ./tools/decode.sh --nj 16 \
+    --beam 15.0 --lattice_beam 7.5 --max_active 7000 \
+    --blank_skip_thresh 0.98 --ctc_weight 0.5 --rescoring_weight 1.0 \
+    --chunk_size $chunk_size \
+    --fst_path data/lang_test/TLG.fst \
+    ${wav_label_dir}data_wenet${job_num}/wav.scp ${wav_label_dir}data_wenet${job_num}/${text_file} $dir/final.zip \
+    data/lang_test/words.txt $dir/Hypo_LM_diff10/${cer_out_dir}_${job_num}
+  now=$(date +"%T")
+  echo "end time : $now"
+fi
+
+if [ ${stage} -le 11 ] && [ ${stop_stage} -ge 11 ]; then
+  chunk_size=-1
+  mode="attention_rescoring"
+  test_dir=$dir/test_${mode}_${job_num}
+  now=$(date +"%T")
+  echo "start time : $now"
+  echo "GPU dir is " $job_num "wav_label_dir is " $wav_label_dir "nj is" 4 "text_file is" $text_file "cer out is" $cer_out_dir "lm is 4gram"
+  echo "dir is " $dir
+
+  ./tools/decode.sh --nj $nj \
+    --beam 15.0 --lattice_beam 7.5 --max_active 7000 \
+    --blank_skip_thresh 0.98 --ctc_weight 0 --rescoring_weight 1 \
+    --chunk_size $chunk_size \
+    --fst_path data/lang_test_4gram/TLG.fst \
+    data/test/wav.scp data/test/text $dir/final.zip \
+    data/lang_test_4gram/words.txt $dir/test_attention_rescoring_withLM_ctc0_rw1
+  now=$(date +"%T")
+  echo "end time : $now"
+fi
+
